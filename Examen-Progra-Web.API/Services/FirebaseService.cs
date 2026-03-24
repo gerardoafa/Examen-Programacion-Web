19080fa9aa0d83347f106cef64598bc5d571dd1f
Heydi <heydialvarado15jya@gmail.com>
2026-03-23 20:08:44 -0600

    feat: agregar FirebaseService y JwtService con configuracion JWT y Firestore

--- Examen-Progra-Web.API/Services/FirebaseService.cs
+++ Examen-Progra-Web.API/Services/FirebaseService.cs
@@ -1,6 +1,21 @@
 ﻿namespace Examen_Progra_Web.API.Services;
 
-public class FirebaseService
-{
-    
-}
+using Google.Cloud.Firestore;
+
+
+
+
+    public class FirebaseService
+    {
+        private readonly FirestoreDb _db;
+
+        public FirebaseService(IConfiguration configuration)
+        {
+            string credentialsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "firebase-credentials.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialsPath);
+            string projectId = configuration["Firebase:ProjectId"]!;
+            _db = FirestoreDb.Create(projectId);
+        }
+
+        public FirestoreDb GetDb() => _db;
+    }
