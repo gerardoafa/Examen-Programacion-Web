5115f3872ed83e4eb4c76e838b8c04765ec3403c
kevbardales206 <kevin_bardales@unitec.edu>
2026-03-23 18:50:36 -0600

    Update Clasificaciones.cs

--- Examen-Progra-Web.API/DTOs/Clasificaciones.cs
+++ Examen-Progra-Web.API/DTOs/Clasificaciones.cs
@@ -1,6 +1,12 @@
-﻿namespace Examen_Progra_Web.API.DTOs;
-
-public class Clasificaciones
+﻿public class ClasificacionDto
 {
-    
+    public string JugadorId { get; set; } = string.Empty;
+    public string JuegoId { get; set; } = string.Empty;
+    public int Posicion { get; set; }
+    public int PuntosJuego { get; set; }
+    public int NivelJuego { get; set; }
+    public double RatioVictoria { get; set; }
+    public int RachaActual { get; set; }
+    public int MedallasOro { get; set; }
+    public List<string> Logros { get; set; } = new();
 }
