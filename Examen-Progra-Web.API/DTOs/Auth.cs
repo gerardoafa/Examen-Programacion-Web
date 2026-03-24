4bf6f2a790d8b30b3d925e7c97162ebb4b856926
Giancarloep <giancarlo_echeverria@unitec.edu>
2026-03-23 19:12:17 -0600

    save de working dtos

--- Examen-Progra-Web.API/DTOs/Auth.cs
+++ Examen-Progra-Web.API/DTOs/Auth.cs
@@ -1,6 +1,28 @@
-﻿namespace Examen_Progra_Web.API.DTOs;
+using Examen_Progra_Web.API.DTOs;
 
-public class Auth
+namespace Examen_Progra_Web.API.DTOs;
+
+public class RegisterDto
 {
-    
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+    public string Contrasena { get; set; } = string.Empty;
+    public string NombreUsuario { get; set; } = string.Empty;
+    public int Edad { get; set; }
+    public string Pais { get; set; } = string.Empty;
+}
+
+public class LoginDto
+{
+    public string Correo { get; set; } = string.Empty;
+    public string Contrasena { get; set; } = string.Empty;
+}
+
+public class AuthResponseDto
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public JugadorDto? Jugador { get; set; }
 }
